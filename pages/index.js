c8d76a93fd0acce77f895a0be424a7b601bac6de
John Jago <johnjago@protonmail.com>
2021-05-10 22:51:04 -0500

    feat: create basic layout of the page

--- pages/index.js
+++ pages/index.js
@@ -5,49 +5,56 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Pitch Card Generator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Pitch Card Generator
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <div className={styles.col}>
+            <h2>Pitch name</h2>
+            <input type="text" tabindex="1" />
+            <input type="text" tabindex="4" />
+            <input type="text" tabindex="7" />
+            <input type="text" tabindex="10" />
+            <input type="text" tabindex="13" />
+            <input type="text" tabindex="16" />
+            <input type="text" tabindex="19" />
+            <input type="text" tabindex="22" />
+            <input type="text" tabindex="25" />
+            <input type="text" tabindex="28" />
+          </div>
+          <div className={styles.col}>
+            <h2>Pitch abbreviation</h2>
+            <input type="text" tabindex="2" />
+            <input type="text" tabindex="5" />
+            <input type="text" tabindex="8" />
+            <input type="text" tabindex="11" />
+            <input type="text" tabindex="14" />
+            <input type="text" tabindex="17" />
+            <input type="text" tabindex="20" />
+            <input type="text" tabindex="23" />
+            <input type="text" tabindex="26" />
+            <input type="text" tabindex="29" />
+          </div>
+          <div className={styles.col}>
+            <h2>Pitch %</h2>
+            <input type="number" tabindex="3" />
+            <input type="number" tabindex="6" />
+            <input type="number" tabindex="9" />
+            <input type="number" tabindex="12" />
+            <input type="number" tabindex="15" />
+            <input type="number" tabindex="18" />
+            <input type="number" tabindex="21" />
+            <input type="number" tabindex="24" />
+            <input type="number" tabindex="27" />
+            <input type="number" tabindex="30" />
+          </div>
         </div>
+        <button className={styles.button}>Make sheets</button>
       </main>
 
       <footer className={styles.footer}>
