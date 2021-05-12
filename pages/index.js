d03d699b28ea7e2fd4e76522e235b9b09a1b396a
John Jago <johnjago@protonmail.com>
2021-05-12 09:19:53 -0500

    feat: calculate total percent
    also: add intuitive tabindex, clean up CSS

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,31 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { useState } from 'react';
 
 export default function Home() {
+  const [percents, setPercents] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const [percentTotal, setPercentTotal] = useState(percents.reduce((acc, cur) => acc + cur, 0));
+
+  const updatePercents = (value, i) => {
+    const percentsCopy = [...percents];
+    percentsCopy[i] = value;
+    setPercents(percentsCopy);
+    calculateTotal(percentsCopy);
+  }
+
+  const calculateTotal = (arr) => {
+    const totalPercent = arr.reduce((total, percent) => {
+      return total + percent;
+    }, 0);
+    setPercentTotal(totalPercent);
+  };
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Pitch Card Generator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className={styles.main}>
         <h1 className={styles.title}>
           Pitch Card Generator
@@ -16,57 +33,43 @@ export default function Home() {
         <div className={styles.grid}>
           <div className={styles.col}>
             <h2>Pitch name</h2>
-            <input type="text" tabindex="1" />
-            <input type="text" tabindex="4" />
-            <input type="text" tabindex="7" />
-            <input type="text" tabindex="10" />
-            <input type="text" tabindex="13" />
-            <input type="text" tabindex="16" />
-            <input type="text" tabindex="19" />
-            <input type="text" tabindex="22" />
-            <input type="text" tabindex="25" />
-            <input type="text" tabindex="28" />
+            {percents && percents.map((p, i) =>
+              <input
+                type="text"
+                tabIndex={i*3 + 1}
+                key={i}
+              />
+            )}
           </div>
           <div className={styles.col}>
             <h2>Pitch abbreviation</h2>
-            <input type="text" tabindex="2" />
-            <input type="text" tabindex="5" />
-            <input type="text" tabindex="8" />
-            <input type="text" tabindex="11" />
-            <input type="text" tabindex="14" />
-            <input type="text" tabindex="17" />
-            <input type="text" tabindex="20" />
-            <input type="text" tabindex="23" />
-            <input type="text" tabindex="26" />
-            <input type="text" tabindex="29" />
+            {percents && percents.map((p, i) =>
+              <input
+                type="text"
+                tabIndex={i*3 + 2}
+                key={i}
+              />
+            )}
           </div>
           <div className={styles.col}>
             <h2>Pitch %</h2>
-            <input type="number" tabindex="3" />
-            <input type="number" tabindex="6" />
-            <input type="number" tabindex="9" />
-            <input type="number" tabindex="12" />
-            <input type="number" tabindex="15" />
-            <input type="number" tabindex="18" />
-            <input type="number" tabindex="21" />
-            <input type="number" tabindex="24" />
-            <input type="number" tabindex="27" />
-            <input type="number" tabindex="30" />
+            {percents && percents.map((p, i) =>
+              <input
+                type="number"
+                tabIndex={i*3 + 3}
+                min="0"
+                max="100"
+                onFocus={e => e.target.select()}
+                key={i}
+                value={percents[i]}
+                onChange={e => updatePercents(Number(e.target.value), i)}
+              />
+            )}
+            <div className={styles.total}>{percentTotal}%</div>
           </div>
         </div>
         <button className={styles.button}>Make sheets</button>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
