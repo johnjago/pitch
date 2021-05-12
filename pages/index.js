c2193858f663a64cd32f3fe469e907d641bfa015
John Jago <johnjago@protonmail.com>
2021-05-12 09:22:46 -0500

    refactor: change indentation to tabs

--- pages/index.js
+++ pages/index.js
@@ -3,73 +3,73 @@ import styles from '../styles/Home.module.css'
 import { useState } from 'react';
 
 export default function Home() {
-  const [percents, setPercents] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
-  const [percentTotal, setPercentTotal] = useState(percents.reduce((acc, cur) => acc + cur, 0));
+	const [percents, setPercents] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+	const [percentTotal, setPercentTotal] = useState(percents.reduce((acc, cur) => acc + cur, 0));
 
-  const updatePercents = (value, i) => {
-    const percentsCopy = [...percents];
-    percentsCopy[i] = value;
-    setPercents(percentsCopy);
-    calculateTotal(percentsCopy);
-  }
+	const updatePercents = (value, i) => {
+		const percentsCopy = [...percents];
+		percentsCopy[i] = value;
+		setPercents(percentsCopy);
+		calculateTotal(percentsCopy);
+	}
 
-  const calculateTotal = (arr) => {
-    const totalPercent = arr.reduce((total, percent) => {
-      return total + percent;
-    }, 0);
-    setPercentTotal(totalPercent);
-  };
+	const calculateTotal = (arr) => {
+		const totalPercent = arr.reduce((total, percent) => {
+			return total + percent;
+		}, 0);
+		setPercentTotal(totalPercent);
+	};
 
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Pitch Card Generator</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Pitch Card Generator
-        </h1>
-        <div className={styles.grid}>
-          <div className={styles.col}>
-            <h2>Pitch name</h2>
-            {percents && percents.map((p, i) =>
-              <input
-                type="text"
-                tabIndex={i*3 + 1}
-                key={i}
-              />
-            )}
-          </div>
-          <div className={styles.col}>
-            <h2>Pitch abbreviation</h2>
-            {percents && percents.map((p, i) =>
-              <input
-                type="text"
-                tabIndex={i*3 + 2}
-                key={i}
-              />
-            )}
-          </div>
-          <div className={styles.col}>
-            <h2>Pitch %</h2>
-            {percents && percents.map((p, i) =>
-              <input
-                type="number"
-                tabIndex={i*3 + 3}
-                min="0"
-                max="100"
-                onFocus={e => e.target.select()}
-                key={i}
-                value={percents[i]}
-                onChange={e => updatePercents(Number(e.target.value), i)}
-              />
-            )}
-            <div className={styles.total}>{percentTotal}%</div>
-          </div>
-        </div>
-        <button className={styles.button}>Make sheets</button>
-      </main>
-    </div>
-  )
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Pitch Card Generator</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<main className={styles.main}>
+				<h1 className={styles.title}>
+					Pitch Card Generator
+				</h1>
+				<div className={styles.grid}>
+					<div className={styles.col}>
+						<h2>Pitch name</h2>
+						{percents && percents.map((p, i) =>
+							<input
+								type="text"
+								tabIndex={i*3 + 1}
+								key={i}
+							/>
+						)}
+					</div>
+					<div className={styles.col}>
+						<h2>Pitch abbreviation</h2>
+						{percents && percents.map((p, i) =>
+							<input
+								type="text"
+								tabIndex={i*3 + 2}
+								key={i}
+							/>
+						)}
+					</div>
+					<div className={styles.col}>
+						<h2>Pitch %</h2>
+						{percents && percents.map((p, i) =>
+							<input
+								type="number"
+								tabIndex={i*3 + 3}
+								min="0"
+								max="100"
+								onFocus={e => e.target.select()}
+								key={i}
+								value={percents[i]}
+								onChange={e => updatePercents(Number(e.target.value), i)}
+							/>
+						)}
+						<div className={styles.total}>{percentTotal}%</div>
+					</div>
+				</div>
+				<button className={styles.button}>Make sheets</button>
+			</main>
+		</div>
+	)
 }
